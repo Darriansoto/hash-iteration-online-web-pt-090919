0076ab30d61e32fdc01c24bb52c05f910d8ecaa8
Darrian Soto <ddog350@gmail.com>
2019-10-08 15:55:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,8 +12,8 @@ end
 
 def age_appropiate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-   if age > 12 
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age > 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
 end
